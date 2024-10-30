cceb9fb6cf8c601bdbb62fa4497eff8a5c8cd427
Vyanktesh <141150425+VyankteshKoli@users.noreply.github.com>
2024-10-30 11:54:55 +0530

    Update GuessGame.cpp

--- GuessGame.cpp
+++ GuessGame.cpp
@@ -6,22 +6,20 @@ int main() {
     int guess;
     int numberOfGuesses = 0;
 
-    cout << "Welcome to Guessing Game!" << endl;
+    cout << "! Welcome to Guessing Game !" << endl;
     cout << "Guess the number between 1 and 100." << endl;
 
-    // Loop until the user guesses the correct number
     do {
-        cout << "Enter your guess: ";
+        cout << "Enter your Guess:- ";
         cin >> guess;
         numberOfGuesses++;
 
-        // Check if the guess is correct, too high, or too low
         if (guess > num) {
-            cout << " It's a high number! Try again." << endl;
+            cout << "It's a High Number! Try again." <<endl;
         } else if (guess < num) {
-            cout << "It's a small number! Try again." << endl;
+            cout << "It's a Small Number! Try again." <<endl;
         } else {
-            cout << "Hurrrahhh! You've guessed the correct number in " << numberOfGuesses << " attempts." << endl;
+            cout << "Hurrrahhh! You've guessed the Correct Number is " << numberOfGuesses << " Attempts." <<endl;
         }
     } while (guess != num);
 
